6cf0d69e99b20797012e694ae3fc9ef62f2f879d
datebackk <caskooss@gmail.com>
2023-04-19 02:19:41 +0300

    feat(frontend-challenge-space): add keycloack example app and login flow

--- apps/frontend/challenge-space/src/app/app.component.ts
+++ apps/frontend/challenge-space/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {KeycloakService} from 'keycloak-angular';
+import {KeycloakProfile} from 'keycloak-js';
 
 @Component({
     selector: 'challenge-space-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'frontend-challenge-space';
+    isLoggedIn = false;
+    userProfile: KeycloakProfile | null = null;
+
+    constructor(private readonly keycloak: KeycloakService) {}
+
+    async ngOnInit(): Promise<void> {
+        this.isLoggedIn = await this.keycloak.isLoggedIn();
+
+        if (this.isLoggedIn) {
+            this.userProfile = await this.keycloak.loadUserProfile();
+        }
+    }
+
+    login() {
+        this.keycloak.login();
+    }
+
+    logout() {
+        this.keycloak.logout();
+    }
 }
--- apps/frontend/challenge-space/src/app/app.module.ts
+++ apps/frontend/challenge-space/src/app/app.module.ts
@@ -1,18 +1,49 @@
-import {NgModule} from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterModule} from '@angular/router';
+import {TUI_SANITIZER, TuiAlertModule, TuiDialogModule, TuiRootModule} from '@taiga-ui/core';
+import {NgDompurifySanitizer} from '@tinkoff/ng-dompurify';
+import {KeycloakAngularModule, KeycloakService} from 'keycloak-angular';
 
 import {AppComponent} from './app.component';
 import {appRoutes} from './app.routes';
-import {NxWelcomeComponent} from './nx-welcome.component';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+    return async () =>
+        keycloak.init({
+            config: {
+                realm: 'master',
+                url: 'http://localhost:8080/auth',
+                clientId: 'challenge-space-front',
+            },
+            initOptions: {
+                checkLoginIframe: true,
+                checkLoginIframeInterval: 25,
+            },
+        });
+}
 
 @NgModule({
-    declarations: [AppComponent, NxWelcomeComponent],
+    declarations: [AppComponent],
     imports: [
         BrowserModule,
         RouterModule.forRoot(appRoutes, {initialNavigation: 'enabledBlocking'}),
+        BrowserAnimationsModule,
+        TuiRootModule,
+        TuiDialogModule,
+        TuiAlertModule,
+        KeycloakAngularModule,
     ],
-    providers: [],
+    providers: [
+        {provide: TUI_SANITIZER, useClass: NgDompurifySanitizer},
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initializeKeycloak,
+            multi: true,
+            deps: [KeycloakService],
+        },
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
